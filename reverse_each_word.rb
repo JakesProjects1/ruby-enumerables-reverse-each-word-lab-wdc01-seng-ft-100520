24f23d0b5ec36663c2c53280b334de0e33c15c0c
Jacob Kagon <jacob@kagon.net>
2020-09-19 18:01:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
-def reverse_each_word(string)
-  string = "Hello there, and how are you?"
-  new_string = string.split("")
+def reverse_each_word(sentence1)
+  new_string = sentence1.split("")
   new_array = []
-  new_array.each do |word|
+  new_string.each do |word|
   new_array << word.reverse
   end
 end
